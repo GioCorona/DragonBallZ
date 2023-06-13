269b33b806358a9869518dc4759d055274fffdde
GioCorona <72942935+GioCorona@users.noreply.github.com>
2023-06-13 14:33:26 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,52 +15,52 @@ function LimpiarCache(cacheName, numeroItems) {
 self.addEventListener("install", (e) => {
 	const cacheProm = caches.open(CacheEstatico).then((cache) => {
 		cache.addAll([
-			"/Los-Gokus/images/camara.png",
-			"/Los-Gokus/images/androide.webp",
-			"/Los-Gokus/images/goku_1.webp",
-			"/Los-Gokus/images/goku_2.jpg",
-			"/Los-Gokus/images/goku_3.webp",
-			"/Los-Gokus/images/goku_4.jpg",
-			"/Los-Gokus/images/goku_5.jpg",
-			"/Los-Gokus/images/goku_6.jfif",
-			"/Los-Gokus/images/vegeta_1.jfif",
-			"/Los-Gokus/images/vegeta_2.webp",
-			"/Los-Gokus/images/vegeta_3.webp",
-			"/Los-Gokus/images/vegeta_4.jpg",
-			"/Los-Gokus/images/krillin.jpg",
-			"/Los-Gokus/images/trunks.jpg",
-			"/Los-Gokus/images/piccolo_1.jpg",
-			"/Los-Gokus/images/piccolo_2.jpg",
-			"/Los-Gokus/images/piccolo_3.jfif",
-			"/Los-Gokus/images/piccolo_4.jpg",
-			"/Los-Gokus/images/dende.jpg",
-			"/Los-Gokus/images/boo_1.jpg",
-			"/Los-Gokus/images/boo_2.jpg",
-			"/Los-Gokus/images/boo_3.jpg",
-			"/Los-Gokus/images/boo_4.jpg",
-			"/Los-Gokus/images/black_1.jfif",
-			"/Los-Gokus/images/black_2.webp",
-			"/Los-Gokus/images/shin.webp",
-			"/Los-Gokus/",
-			"/Los-Gokus/index.html",
-			"/Los-Gokus/css/style.css",
-			"/Los-Gokus/js/app.js",
-			"/Los-Gokus/js/script.js",
-			"/Los-Gokus/res/pages/descripciones.html",
+			"/DragonBallZ/images/camara.png",
+			"/DragonBallZ/images/androide.webp",
+			"/DragonBallZ/images/goku_1.webp",
+			"/DragonBallZ/images/goku_2.jpg",
+			"/DragonBallZ/images/goku_3.webp",
+			"/DragonBallZ/images/goku_4.jpg",
+			"/DragonBallZ/images/goku_5.jpg",
+			"/DragonBallZ/images/goku_6.jfif",
+			"/DragonBallZ/images/vegeta_1.jfif",
+			"/DragonBallZ/images/vegeta_2.webp",
+			"/DragonBallZ/images/vegeta_3.webp",
+			"/DragonBallZ/images/vegeta_4.jpg",
+			"/DragonBallZ/images/krillin.jpg",
+			"/DragonBallZ/images/trunks.jpg",
+			"/DragonBallZ/images/piccolo_1.jpg",
+			"/DragonBallZ/images/piccolo_2.jpg",
+			"/DragonBallZ/images/piccolo_3.jfif",
+			"/DragonBallZ/images/piccolo_4.jpg",
+			"/DragonBallZ/images/dende.jpg",
+			"/DragonBallZ/images/boo_1.jpg",
+			"/DragonBallZ/images/boo_2.jpg",
+			"/DragonBallZ/images/boo_3.jpg",
+			"/DragonBallZ/images/boo_4.jpg",
+			"/DragonBallZ/images/black_1.jfif",
+			"/DragonBallZ/images/black_2.webp",
+			"/DragonBallZ/images/shin.webp",
+			"/DragonBallZ/",
+			"/DragonBallZ/index.html",
+			"/DragonBallZ/css/style.css",
+			"/DragonBallZ/js/app.js",
+			"/DragonBallZ/js/script.js",
+			"/DragonBallZ/res/pages/descripciones.html",
 		]);
 	});
 	//cache inmutable no se modifica
 	const cacheInm = caches.open(CacheInmutable).then((cache) => {
 		cache.addAll([
-			"/Los-Gokus/css/bootstrap.min.css",
-			"/Los-Gokus/css/fontawesome.min.css",
-			"/Los-Gokus/js/bootstrap.bundle.min.js",
-			"/Los-Gokus/js/jquery.min.js",
-			"/Los-Gokus/js/fontawesome.min.js",
-			"/Los-Gokus/res/pages/404.html",
-			"/Los-Gokus/res/404.png",
-			"/Los-Gokus/images/favicons/favicon.png",
-			"/Los-Gokus/manifest.json",
+			"/DragonBallZ/css/bootstrap.min.css",
+			"/DragonBallZ/css/fontawesome.min.css",
+			"/DragonBallZ/js/bootstrap.bundle.min.js",
+			"/DragonBallZ/js/jquery.min.js",
+			"/DragonBallZ/js/fontawesome.min.js",
+			"/DragonBallZ/res/pages/404.html",
+			"/DragonBallZ/res/404.png",
+			"/DragonBallZ/images/favicons/favicon.png",
+			"/DragonBallZ/manifest.json",
 		]);
 	});
 	e.waitUntil(Promise.all([cacheProm, cacheInm]));
@@ -79,9 +79,9 @@ self.addEventListener("fetch", (e) => {
 					.then((newRes) => {
 						if (!newRes) {
 							if (/\.(png|jpg|webp|jfif)$/.test(e.request.url)) {
-								return caches.match("/Los-Gokus/res/404.png");
+								return caches.match("/DragonBallZ/res/404.png");
 							}
-							return caches.match("/Los-Gokus/res/pages/404.html");
+							return caches.match("/DragonBallZ/res/pages/404.html");
 						}
 						return newRes;
 					});
@@ -105,9 +105,9 @@ self.addEventListener("fetch", (e) => {
 					if (!newRes) {
 						if (/\.(png|jpg|webp|jfif)$/.test(e.request.url)) {
 							console.log("Error de imagen: " + e.request.url);
-							return caches.match("/Los-Gokus/res/404.png");
+							return caches.match("/DragonBallZ/res/404.png");
 						}
-						return caches.match("/Los-Gokus/res/pages/404.html");
+						return caches.match("/DragonBallZ/res/pages/404.html");
 					}
 					return newRes;
 				});
